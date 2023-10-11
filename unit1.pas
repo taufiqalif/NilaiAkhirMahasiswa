752747acb890b5e4aa5a3c0fe33a411920d7fa8d
taufiqalif <aliftaufiq4@gmail.com>
2023-10-11 22:28:19 +0700

    membuat penilaian akhir mahasiswa

---
+++ unit1.pas
@@ -0,0 +1,61 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls;
+
+type
+
+  { TfrmUtama }
+
+  TfrmUtama = class(TForm)
+    btnHitung: TButton;
+    edtNilai: TEdit;
+    edtAbsen: TEdit;
+    edtUts: TEdit;
+    edtUas: TEdit;
+    edtHasil: TEdit;
+    lblJudul: TLabel;
+    lblNilai: TLabel;
+    lblAbsen: TLabel;
+    lblUts: TLabel;
+    lblUas: TLabel;
+    lblHasil: TLabel;
+    procedure btnHitungClick(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  frmUtama: TfrmUtama;
+
+implementation
+
+{$R *.lfm}
+
+{ TfrmUtama }
+
+procedure TfrmUtama.btnHitungClick(Sender: TObject);
+var
+  Tugas, Absensi, UTS, UAS, NilaiAkhir: Double;
+begin
+  // Konversi input pengguna menjadi bilangan pecahan (Double)
+  Tugas := StrToFloat(edtNilai.Text);
+  Absensi := StrToFloat(edtAbsen.Text);
+  UTS := StrToFloat(edtUts.Text);
+  UAS := StrToFloat(edtUas.Text);
+
+  // Hitung nilai akhir berdasarkan persentase
+  NilaiAkhir := (0.2 * Tugas) + (0.1 * Absensi) + (0.3 * UTS) + (0.4 * UAS);
+
+  // Tampilkan nilai akhir dalam kotak teks Hasil
+  edtHasil.Text := FloatToStr(NilaiAkhir);
+end;
+
+end.
+
